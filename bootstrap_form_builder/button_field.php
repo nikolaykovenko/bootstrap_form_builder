ab14547355ec113c077bab2488d26b98bd9514e2
Nikolay Kovenko <nikolay.kovenko@gmail.com>
2014-01-30 09:41:36 +0200

    Добавлено поле кнопки

---
+++ bootstrap_form_builder/button_field.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package bootstrap_form_builder
+ * @author Nikolay Kovenko <nikolay.kovenko@gmail.com>
+ * @date 17.01.14
+ */
+
+namespace bootstrap_form_builder;
+
+/**
+ * Кнопка формы
+ * @package bootstrap_form_builder
+ */
+class button_field extends a_control_field {
+ /**
+  * @param string $value значение поля
+  */
+ public function __construct($value)
+ {
+  $this->set_value($value);
+  $this->add_element_type_classes();
+ }
+
+ public function render_field()
+ {
+  return '<input'.$this->get_summary().' type="'.$this->get_type().'" value="'.htmlspecialchars($this->get_value()).'">';
+ }
+
+ public function get_type()
+ {
+  return 'button';
+ }
+
+ public function add_element_type_classes()
+ {
+  $this->add_class('btn');
+ }
+}
--- bootstrap_form_builder/submit_field.php
+++ bootstrap_form_builder/submit_field.php
@@ -11,20 +11,7 @@ namespace bootstrap_form_builder;
  * Кнопка отправки формы
  * @package bootstrap_form_builder
  */
-class submit_field extends a_control_field {
- /**
-  * @param string $value значение поля
-  */
- public function __construct($value)
- {
-  $this->set_value($value);
-  $this->add_element_type_classes();
- }
- 
- public function render_field()
- {
-  return '<input'.$this->get_summary().' type="'.$this->get_type().'" value="'.htmlspecialchars($this->get_value()).'">';
- }
+class submit_field extends button_field {
  
  public function get_type()
  {
@@ -33,6 +20,7 @@ class submit_field extends a_control_field {
 
  public function add_element_type_classes()
  {
-  $this->add_class('btn btn-primary');
+  parent::add_element_type_classes();
+  $this->add_class('btn-primary');
  }
 }
